49738c93493fa0a2f1db16e8f1fd1b908306531d
rosangelalins <rosangela.mslins@gmail.com>
2021-05-09 16:09:44 -0300

    adiciona  HTML5, CSS, JavaScript, JQuery e imagens

---
+++ js/owl/setup.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true, //a passagem dos filme fica infinita, 
+    //caso coloque false, ele chega a um certo limite para o lado
+    margin:10, //distancia de um filme para o outro
+    nav:false, //o menu pode ser desabilitado que fica em baixo dos filmes com false ou habilitado true
+    responsive:{ //adaptação dos elementos para o tamanho da tela
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
